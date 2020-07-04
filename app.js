f17e58506647134e52affd6d29e4f16d15debae6
hsegni <hajer.segni@gmail.com>
2020-07-04 15:35:55 +0100

    first commit

--- app.js
+++ app.js
@@ -1,74 +1,60 @@
 (function() {
-  'use strict';
+    'use strict';
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  angular.module('NarrowItDownApp', [])
-      .controller('NarrowItDownController', NarrowItDownController)
-      .service('MenuSearchService', MenuSearchService)
-      .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-      .directive('foundItems', FoundItems);
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
 
-  function FoundItems() {
-      var ddo = {
-          restrict: 'E',
-          templateUrl: 'loader/foundItems.html',
-          scope: {
-              foundItems: '<',
-              onEmpty: '<',
-              onRemove: '&'
-          },
-          controller: NarrowItDownController,
-          controllerAs: 'menu',
-          bindToController: true
-      };
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuy = this;
+        toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+        toBuy.checkOff = function(itemIndex) {
+            ShoppingListCheckOffService.checkOff(itemIndex);
+        }
+    }
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
-      return ddo;
-  }
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var bought = this;
+        bought.items = ShoppingListCheckOffService.getBoughtItems();
+    }
 
-  NarrowItDownController.$inject = ['MenuSearchService'];
-
-  function NarrowItDownController(MenuSearchService) {
-      var menu = this;
-      menu.shortName = '';
-
-      menu.matchedMenuItems = function(searchTerm) {
-          var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-
-          promise.then(function(items) {
-              if (items && items.length > 0) {
-                  menu.message = '';
-                  menu.found = items;
-              } else {
-                  menu.message = 'Nothing found!';
-                  menu.found = [];
-              }
-          });
-      };
-
-      menu.removeMenuItem = function(itemIndex) {
-          menu.found.splice(itemIndex, 1);
-      }
-  }
-
-  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-
-  function MenuSearchService($http, ApiBasePath) {
-      var service = this;
-
-      service.getMatchedMenuItems = function(searchTerm) {
-          return $http({
-              method: "GET",
-              url: (ApiBasePath + "/menu_items.json")
-          }).then(function(response) {
-              var foundItems = [];
-
-              for (var i = 0; i < response.data['menu_items'].length; i++) {
-                  if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
-                      foundItems.push(response.data['menu_items'][i]);
-                  }
-              }
-
-              return foundItems;
-          });
-      };
-  }
+    function ShoppingListCheckOffService() {
+        var service = this;
+        var toBuyItems = [{
+            name: "paprika",
+            quantity: "5"
+        }, {
+            name: "cookies",
+            quantity: "10"
+        },{
+            name: "fruits",
+            quantity: "10"
+        }, {
+            name: "cakes",
+            quantity: "25"
+        }, {
+            name: "apples",
+            quantity: " 5"
+        }, {
+            name: "tomatos",
+            quantity: " 10"
+        }, {
+            name: "chocolates",
+            quantity: "Pack of 25"
+        }];
+        var boughtItems = [];
+        service.getToBuyItems = function() {
+            return toBuyItems;
+        };
+        service.getBoughtItems = function() {
+            return boughtItems;
+        };
+        service.checkOff = function(itemIndex) {
+            boughtItems.push(toBuyItems[itemIndex]);
+            toBuyItems.splice(itemIndex, 1);
+        };
+    }
 })();
