c2e8abb017e54cc89e4fd6a9fa40e882212c250c
hsegni <hajer.segni@gmail.com>
2020-07-04 13:35:47 +0100

    first version

--- app.js
+++ app.js
@@ -1,61 +1,74 @@
 (function() {
-    "use strict";
-  
-    angular.module("ShoppingListCheckOff", [])
-      .controller("ToBuyShoppingController", ToBuyShoppingController)
-      .controller("AlreadyBoughtShoppingController", AlreadyBoughtShoppingController)
-      .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
-  
-  
-    ToBuyShoppingController.$inject = ["ShoppingListCheckOffService"];
-    function ToBuyShoppingController(ShoppingListCheckOffService) {
-      var list = this;
-  
-      list.items = ShoppingListCheckOffService.getItemsToBuy();
-  
-      list.checkOff = function(itemIndex) {
-        console.log("Checking off item ", itemIndex);
-        ShoppingListCheckOffService.checkOff(itemIndex);
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+      .controller('NarrowItDownController', NarrowItDownController)
+      .service('MenuSearchService', MenuSearchService)
+      .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+      .directive('foundItems', FoundItems);
+
+  function FoundItems() {
+      var ddo = {
+          restrict: 'E',
+          templateUrl: 'loader/foundItems.html',
+          scope: {
+              foundItems: '<',
+              onEmpty: '<',
+              onRemove: '&'
+          },
+          controller: NarrowItDownController,
+          controllerAs: 'menu',
+          bindToController: true
       };
-    }
-  
-  
-    AlreadyBoughtShoppingController.$inject = ["ShoppingListCheckOffService"];
-    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
-      var list = this;
-  
-      list.items = ShoppingListCheckOffService.getItemsBought();
-    }
-  
-  
-    // ShoppingListCheckOffService.$inject = [];
-    function ShoppingListCheckOffService() {
+
+      return ddo;
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+
+  function NarrowItDownController(MenuSearchService) {
+      var menu = this;
+      menu.shortName = '';
+
+      menu.matchedMenuItems = function(searchTerm) {
+          var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+          promise.then(function(items) {
+              if (items && items.length > 0) {
+                  menu.message = '';
+                  menu.found = items;
+              } else {
+                  menu.message = 'Nothing found!';
+                  menu.found = [];
+              }
+          });
+      };
+
+      menu.removeMenuItem = function(itemIndex) {
+          menu.found.splice(itemIndex, 1);
+      }
+  }
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
+  function MenuSearchService($http, ApiBasePath) {
       var service = this;
-  
-      var itemsToBuy = [ 
-        { name: "Apples", quantity: 1 }, 
-        { name: "Paprika", quantity:10 }, 
-        { name: "tomatos", quantity: 3 }, 
-        { name: "frites", quantity: 2 }, 
-        { name: "sucker", quantity: 5 }, 
-        { name: "lasagna", quantity: 6 }, 
-        { name: "pizzas", quantity: 7 }, 
-       
-      ];
-  
-      var itemsBought = [];
-  
-      service.getItemsToBuy = function () {
-        return itemsToBuy;
+
+      service.getMatchedMenuItems = function(searchTerm) {
+          return $http({
+              method: "GET",
+              url: (ApiBasePath + "/menu_items.json")
+          }).then(function(response) {
+              var foundItems = [];
+
+              for (var i = 0; i < response.data['menu_items'].length; i++) {
+                  if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
+                      foundItems.push(response.data['menu_items'][i]);
+                  }
+              }
+
+              return foundItems;
+          });
       };
-  
-      service.getItemsBought = function () {
-        return itemsBought;
-      };
-  
-      service.checkOff = function (itemIndex) {
-        itemsBought.push(itemsToBuy[itemIndex]);
-        itemsToBuy.splice(itemIndex, 1);
-      };
-    }
-  })();
+  }
+})();
